c19b9d04f34472195d51531fb854c3e27509a3e7
Papweer <158085165+Papweer@users.noreply.github.com>
2024-12-13 09:11:37 +0000

    Deploying to gh-pages from @ Papweer/godotGames@1d8e7025b13c8f20f9508f82f6860a82c746ee24 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734080542|8195596';
+const CACHE_VERSION = '1734081085|8255522';
 /** @type {string} */
 const CACHE_PREFIX = '2d-towerdefenseg-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
